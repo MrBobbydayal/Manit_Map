b83f0837b3c28dbc1dd7d4717e14f2928c71c449
MrBobbydayal <bobbydayal398@gmail.com>
2025-04-13 19:33:13 +0530

    commit search fuction

--- src/App.jsx
+++ src/App.jsx
@@ -1,120 +1,180 @@
-import { useRef, useEffect ,useState} from 'react'
-import mapboxgl from 'mapbox-gl'
-import 'mapbox-gl/dist/mapbox-gl.css';
-// // Import Geocoder styles (must come before map load)
-// import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+import { useRef, useEffect, useState } from "react";
+import { SearchBox } from "@mapbox/search-js-react";
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
-// // Import the Geocoder control itself
-// import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+const accessToken = "pk.eyJ1IjoiMjIxMTEwMTEzMiIsImEiOiJjbTc3cWhldXIxMmZyMnJzZ3F4ZGp6YTZuIn0.vOl6F3cJgJ0qV505fDe58w";
 
-import './App.css'
-import { SearchBox } from '@mapbox/search-js-react';
-import MyAddressForm from'./component/AdressAutofill.jsx';
-import { Geocoder } from '@mapbox/search-js-react';
-
-function App() {
-  const mapRef = useRef()
-  const mapContainerRef = useRef()
-
-   const [layer,setlayer]=useState()
-  // mapRef.current = new mapboxgl.Map({
-  //   container: mapContainerRef.current,
-  //   center: [-74.0242, 40.6941],
-  //   zoom: 10.12
-  // });
-
-  // useEffect(() => {
-  //   mapboxgl.accessToken = "pk.eyJ1IjoiMjIxMTEwMTEzMiIsImEiOiJjbTc3cWhldXIxMmZyMnJzZ3F4ZGp6YTZuIn0.vOl6F3cJgJ0qV505fDe58w"
-  //   mapRef.current = new mapboxgl.Map({
-  //     container: mapContainerRef.current,
-  //   });
-
-  //   return () => {
-  //     mapRef.current.remove()
-  //   }
-  // }, [])
-
-
+export default function MapWithGeocoder() {
+  const mapContainerRef = useRef();
+  const mapInstanceRef = useRef();
+  const [mapLoaded, setMapLoaded] = useState(false);
+  const [inputValue, setInputValue] = useState("");
   useEffect(() => {
-    mapboxgl.accessToken = 'pk.eyJ1IjoiMjIxMTEwMTEzMiIsImEiOiJjbTc3cWhldXIxMmZyMnJzZ3F4ZGp6YTZuIn0.vOl6F3cJgJ0qV505fDe58w';
+    mapboxgl.accessToken = accessToken;
 
-    mapRef.current = new mapboxgl.Map({
-      container: mapContainerRef.current,
-    // style: 'mapbox://styles/2211101132/cm7p4p52i003z01sc6s4e3wvf',
-     //style:'mapbox://styles/2211101132/cm7ydisg501nx01s29d7i4jnd',
-     style:'mapbox://styles/2211101132/cm937phgq009301qq0mh00y8g',
-      zoom: 1.5,
-      center: [77.406111111,23.21472222]
+    mapInstanceRef.current = new mapboxgl.Map({
+      container: mapContainerRef.current, // container ID
+      
+       //style:'mapbox://styles/2211101132/cm937phgq009301qq0mh00y8g',//style for all layer digitiged by me in sattelite mode
+      //style:'mapbox://styles/mapbox/light-v11',//for backround not showing
+      style:'mapbox://styles/2211101132/cm937phgq009301qq0mh00y8g',//style for all layer digitiged by me in custome mode
+      center: [77.406111111,23.21472222], // starting position [lng, lat]
+      zoom: 13, // starting zoom
     });
-    mapRef.current.on('load', () => {
-      mapRef.current.addSource('mapbox-terrain', {
-        type: 'vector',
-         url:'mapbox://2211101132.manit-tiles'
-         //2211101132.42jzv3rf'
-      });
-      mapRef.current.addLayer(
-        {
-          id: 'terrain-data',
-          type: 'line',
-          source: 'mapbox-terrain',
-          'source-layer': 'manit',
-          layout: {
-            'line-join': 'round',
-            'line-cap': 'round'
-          },
-          paint: {
-            'line-color': '#ff69b4',
-            'line-width': 1
-          }
-        },
-        'road-label-simple'
-      );
+      
 
-
-    //   //real check
-    //   mapRef.current.addControl(new mapboxgl.NavigationControl(), 'top-right');
-
-    // // Add geocoder (search box)
-    // const geocoder = new MapboxGeocoder({
-    //   accessToken: mapboxgl.accessToken,
-    //   mapboxgl: mapboxgl,
-    //   marker: true, // Automatically add marker at result
-    //   placeholder: 'Search for a location',
-    // });
-
-    // mapRef.current.addControl(geocoder, 'top-left');
+    mapInstanceRef.current.on("load", () => {
+      setMapLoaded(true);
     });
-
-    return () => mapRef.current.remove();
   }, []);
 
-
-
-
-
-
-
-
   return (
-    <>
-    
-    <div className="flex-col justify-self-end w-52 bg-white border-black bg-neutral-500  space-y-4">
-    {/* <SearchBox className="border-black bg-slate-500 "/> */}
-    <MyAddressForm className=" text-black bg-stone-700 border-red-700"/>
-      <div className='bg-slate-100 flex-col space-y-4'>
-    <button className='text-slate-200 bg-teal-900 w-32 h-10 rounded-lg' >sattelite</button>
-    <button className='text-slate-200 bg-teal-900 w-32 h-10 rounded-lg' >custom</button>
-    </div>
+    <><div className="">
+      <div className=" flex-row inline-flex space-x-4">
+      <SearchBox
+        accessToken={accessToken}
+        map={mapInstanceRef.current}
+        mapboxgl={mapboxgl}
+        value={inputValue}
+        onChange={(d) => {
+          setInputValue(d);
+        }}
+        marker
+      />
+      
+      <input type="string" placeholder="Origin" className="px-3 py-2 border border-gray-300 rounded-md cursor-text h-9 w-60 "/>
+      
+      <input type="string" placeholder="Destination" className="px-3 py-2 border border-gray-300 rounded-md cursor-text h-9 w-60"/>
 
-
-    </div>
-    <div id='map-container' ref={mapContainerRef}/>
-    
+      </div>
+      <div className="place-items-end ">
+      <div id="map-container" ref={mapContainerRef} style={{ height:650,width:1500 }} className="items-end" />
+      </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//For showing Map with adding layer
+
+
+// import { useRef, useEffect ,useState} from 'react'
+// import mapboxgl from 'mapbox-gl'
+// import 'mapbox-gl/dist/mapbox-gl.css';
+// import './App.css';
+// import MyAddressForm from'./component/AdressAutofill.jsx';
+
+
+// function App() {
+//   const mapRef = useRef()
+//   const mapContainerRef = useRef()
+
+
+
+//   useEffect(() => {
+//     mapboxgl.accessToken = 'pk.eyJ1IjoiMjIxMTEwMTEzMiIsImEiOiJjbTc3cWhldXIxMmZyMnJzZ3F4ZGp6YTZuIn0.vOl6F3cJgJ0qV505fDe58w';
+
+//     mapRef.current = new mapboxgl.Map({
+//       container: mapContainerRef.current,
+//       style:'mapbox://styles/mapbox/light-v11',//for backround not showing
+//      //style:'mapbox://styles/2211101132/cm7ydisg501nx01s29d7i4jnd',//with boundary layer but in specific manner
+//      //style:'mapbox://styles/2211101132/cm937phgq009301qq0mh00y8g',//style for all layer digitiged by me
+//       zoom: 13,
+//       center: [77.406111111,23.21472222]
+//     });
+//     mapRef.current.on('load', () => {
+//       mapRef.current.addSource('mapbox-terrain', {
+//         type: 'vector',
+//          url:'mapbox://2211101132.manit-tiles'
+         
+//       });
+//       mapRef.current.addLayer(
+//         {
+//           id: 'terrain-data',
+//           type: 'line',
+//           source: 'mapbox-terrain',
+//           'source-layer': 'manit',
+//           layout: {
+//             'line-join': 'round',
+//             'line-cap': 'round'
+//           },
+//           paint: {
+//             'line-color': '#ff69b4',
+//             'line-width': 1
+//           }
+//         },
+//         'road-label-simple'
+//       );
+//         });
+
+//     return () => mapRef.current.remove();
+//   }, []);
+
+
+
+
+
+
+
+
+//   return (
+//     <>
+    
+//     <div className="flex-col justify-self-end w-52 bg-white border-black bg-neutral-500  space-y-4">
+//     <MyAddressForm className=" text-black bg-stone-700 border-red-700"/>
+//       <div className='bg-slate-100 flex-col space-y-4'>
+//     <button className='text-slate-200 bg-teal-900 w-32 h-10 rounded-lg' >sattelite</button>
+//     <button className='text-slate-200 bg-teal-900 w-32 h-10 rounded-lg' >custom</button>
+//     </div>
+
+
+//     </div>
+//     <div id='map-container' ref={mapContainerRef}/>
+    
+//     </>
+//   )
+// }
+
+// export default App
 
 
 
@@ -132,3 +192,43 @@ mapbox.mapbox-terrain-v2
 
 
 */
+
+
+
+//For Only showing Glob 
+
+// useEffect(() => {
+//   mapboxgl.accessToken = "pk.eyJ1IjoiMjIxMTEwMTEzMiIsImEiOiJjbTc3cWhldXIxMmZyMnJzZ3F4ZGp6YTZuIn0.vOl6F3cJgJ0qV505fDe58w"
+// mapRef.current = new mapboxgl.Map({
+//    container: mapContainerRef.current,
+//    center: [-74.0242, 40.6941],
+//    zoom: 10.12
+//  });
+
+//   return () => {
+//     mapRef.current.remove()
+//   }
+// }, []);
+
+
+
+
+
+// /For GeoCoding
+// // Import Geocoder styles (must come before map load)
+// import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+
+// // Import the Geocoder control itself
+// import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+//   //real check
+//       mapRef.current.addControl(new mapboxgl.NavigationControl(), 'top-right');
+
+//     // Add geocoder (search box)
+//     const geocoder = new MapboxGeocoder({
+//       accessToken: mapboxgl.accessToken,
+//       mapboxgl: mapboxgl,
+//       marker: true, // Automatically add marker at result
+//       placeholder: 'Search for a location',
+//     });
+
+//     mapRef.current.addControl(geocoder, 'top-left');
--- src/component/Geocodding.jsx
+++ src/component/Geocodding.jsx
@@ -1,33 +1,145 @@
-import { Geocoder } from "@mapbox/search-js-react";
-export function Component() {
-  const [value, setValue] = React.useState('');
+// I'll explain how to add navigation functionality to a React web application with Mapbox search functionality.
 
-  const handleChange = (d) => {
-    setValue(d);
-  };
-  return (
-    <Geocoder
-      options={{
-        proximity: {
-          lng: -122.431297,
-          lat: 37.773972,
-        },
-      }}
-      value={value}
-      onChange={handleChange}
-      accessToken="pk.eyJ1IjoiMjIxMTEwMTEzMiIsImEiOiJjbTc3cWhldXIxMmZyMnJzZ3F4ZGp6YTZuIn0.vOl6F3cJgJ0qV505fDe58w"
-    />
-  );
-}
+// ## Adding Navigation to React with Mapbox Search
 
+// For a React web application, you can combine Mapbox's search capabilities with navigation functionality using the Mapbox Directions API. Here's how to implement this:
 
+// ### 1. Set up Search Functionality
 
+// First, you need to implement search functionality using Mapbox Search JS for React:
 
+// ```javascript
+// import { SearchBox } from '@mapbox/search-js-react';
+// import mapboxgl from 'mapbox-gl';
+// import 'mapbox-gl/dist/mapbox-gl.css';
+// import { useRef, useEffect, useState } from "react";
 
-// import { useGeocodingCore } from '@mapbox/search-js-react';
-// const geocodingCore = useGeocodingCore({ accessToken: 'pk.eyJ1IjoiMjIxMTEwMTEzMiIsImEiOiJjbTc3cWhldXIxMmZyMnJzZ3F4ZGp6YTZuIn0.vOl6F3cJgJ0qV505fDe58w' });
-// const response = await geocodingCore.forward('1600 pennsylvania ave nw', {
-//   limit: 1
-// });
-// console.log(response);
-// // { type: 'FeatureCollection', features: [...], attribution: '...', url: '...' };
+// const accessToken = "YOUR_MAPBOX_ACCESS_TOKEN";
+
+// function MapWithSearchAndNavigation() {
+//   const mapContainerRef = useRef();
+//   const mapInstanceRef = useRef();
+//   const [mapLoaded, setMapLoaded] = useState(false);
+//   const [inputValue, setInputValue] = useState("");
+  
+//   useEffect(() => {
+//     mapboxgl.accessToken = accessToken;
+  
+//     mapInstanceRef.current = new mapboxgl.Map({
+//       container: mapContainerRef.current,
+//       style: 'mapbox://styles/mapbox/streets-v11',
+//       center: [-74.5, 40],
+//       zoom: 9,
+//     });
+  
+//     mapInstanceRef.current.on("load", () => {
+//       setMapLoaded(true);
+//     });
+//   }, []);
+  
+//   return (
+//     <>
+//       <SearchBox
+//         accessToken={accessToken}
+//         map={mapInstanceRef.current}
+//         mapboxgl={mapboxgl}
+//         value={inputValue}
+//         onChange={(d) => {
+//           setInputValue(d);
+//         }}
+//         marker
+//       />
+//       <div id="map-container" ref={mapContainerRef} style={{ height: 500 }} />
+//     </>
+//   );
+// }
+// ```
+
+// This code sets up a basic map with the SearchBox component from [Mapbox Search JS React](https://docs.mapbox.com/mapbox-search-js/guides/search/react/).
+
+// ### 2. Add Navigation Functionality
+
+// To add navigation, you'll need to use the Mapbox Directions control. Add the Mapbox GL Directions plugin to your project:
+
+// ```javascript
+// import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions';
+// import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css';
+
+// // Inside your useEffect, after the map loads:
+// useEffect(() => {
+//   mapboxgl.accessToken = accessToken;
+
+//   mapInstanceRef.current = new mapboxgl.Map({
+//     container: mapContainerRef.current,
+//     style: 'mapbox://styles/mapbox/streets-v11',
+//     center: [-74.5, 40],
+//     zoom: 9,
+//   });
+
+//   mapInstanceRef.current.on("load", () => {
+//     // Add the directions control
+//     const directions = new MapboxDirections({
+//       accessToken: mapboxgl.accessToken,
+//       unit: 'metric',
+//       profile: 'mapbox/driving',
+//       controls: {
+//         instructions: true
+//       }
+//     });
+
+//     mapInstanceRef.current.addControl(directions, 'top-left');
+//     setMapLoaded(true);
+//   });
+// }, []);
+// ```
+
+// ### 3. Connect Search Results to Navigation
+
+// To connect your search results to the navigation functionality, you can modify the SearchBox component to set the destination in the Directions control when a search result is selected:
+
+// ```javascript
+// import { SearchBox } from '@mapbox/search-js-react';
+
+// // Inside your component:
+// const [directionsControl, setDirectionsControl] = useState(null);
+
+// // In your useEffect:
+// useEffect(() => {
+//   // ...map initialization code...
+
+//   mapInstanceRef.current.on("load", () => {
+//     const directions = new MapboxDirections({
+//       accessToken: mapboxgl.accessToken,
+//       unit: 'metric',
+//       profile: 'mapbox/driving'
+//     });
+
+//     mapInstanceRef.current.addControl(directions, 'top-left');
+//     setDirectionsControl(directions);
+//     setMapLoaded(true);
+//   });
+// }, []);
+
+// // Handle search result selection
+// const handleSearchResult = (result) => {
+//   if (directionsControl && result.coordinates) {
+//     // Set the destination in the directions control
+//     directionsControl.setDestination(result.coordinates);
+//   }
+// };
+
+// // In your JSX:
+// <SearchBox
+//   accessToken={accessToken}
+//   map={mapInstanceRef.current}
+//   mapboxgl={mapboxgl}
+//   value={inputValue}
+//   onChange={setInputValue}
+//   onRetrieve={handleSearchResult}
+//   marker
+// />
+// ```
+
+// This approach allows users to search for a location and then automatically set it as the destination for navigation.
+
+// For more detailed implementation, you can refer to the [Mapbox Search JS React documentation](https://docs.mapbox.com/mapbox-search-js/guides/search/react/) and examples of using Mapbox GL JS with React from the [Mapbox documentation](https://docs.mapbox.com/help/dive-deeper/web-apps/#use-mapbox-gl-js-with-react).
